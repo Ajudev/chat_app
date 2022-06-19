2782e37e2fb6b44d3f5990415a94d0a15f4b7aff
ajudevnair <ajudevnair@gmail.com>
2022-06-19 23:12:29 +0400

    Initial commit

--- chatapp/chatapp/settings/base.py
+++ chatapp/chatapp/settings/base.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'channels',
+
     # Apps
     'users.apps.UsersConfig',
     'chat_logic.apps.ChatLogicConfig',
